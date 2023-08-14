9bb4dd5e521dd02bcbbbeec01c0268b42c402f15
Samuel Mateus <samuelmateuscardoso@gmail.com>
2023-08-14 12:09:55 -0300

    commit

---
+++ VariaveiseCondicionais/contandoqtdstrings.py
@@ -0,0 +1,5 @@
+entrada_usuario = input("Digite qualquer palavra: ").strip()
+
+quantidade_caracteres = len(entrada_usuario)
+
+print(f"A quantidade de caracteres digitados é de : {quantidade_caracteres} unidades")
