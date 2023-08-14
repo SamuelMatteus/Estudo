88a3fa893bb2346e4c6cc7d10bfbbc3de89d9d85
Samuel Mateus <samuelmateuscardoso@gmail.com>
2023-08-14 20:56:40 -0300

    Exempl básico de implantação de dicionário em Python.

---
+++ Dicionarios/exemplodecadastro.py
@@ -0,0 +1,13 @@
+cadastro = {
+    'id': 1,
+    'nome': 'Samuel Mateus',
+    'filhos': ['Nenhum'],
+    'compras': [
+        {
+            'id': 4758,
+            'produto': 'Notebook Gamer',
+            'preco': 2597.99
+        }
+
+    ]
+}
