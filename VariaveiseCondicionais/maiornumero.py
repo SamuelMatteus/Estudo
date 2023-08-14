5a01751ca6ef50dd381f69e137d323b0fd2deb19
Samuel Mateus <samuelmateuscardoso@gmail.com>
2023-08-14 15:38:20 -0300

    Descobrindo qual é o maior número

---
+++ VariaveiseCondicionais/maiornumero.py
@@ -0,0 +1,6 @@
+numero1 = float(input("Insira o primeiro número: "))
+numero2 = float(input("Insira o segundo número: "))
+numero3 = float(input("Insira o terceiro número: "))
+
+maior_numero = max(numero1,numero2,numero3)
+print(f"O maior número é {maior_numero}")
