d017b2cf9568f73590f2079a29f3573e5a0fe07d
Samuel Mateus <samuelmateuscardoso@gmail.com>
2023-08-28 13:23:30 -0300

    Jogo de forca e adivinhação.

--- ExercíciosAlura/JogoDaForca/menuforca.py
+++ ExercíciosAlura/JogoDaForca/menuforca.py
@@ -3,26 +3,35 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while (not enforcou and not acertou):
 
         chute = input("Qual letra?")
-        chute = chute.strip()
-        indice = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[indice] = letra
+        chute = chute.strip().upper()
 
-                indice = indice + 1
+        if (chute in palavra_secreta):
+            indice = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[indice] = letra
+                indice += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-
+    if acertou:
+        print("Parabéns! Você ganhou!")
+    else:
+        print("Lamento, você perdeu...")
     print("Fim de jogo")
 
 if(__name__ == "__main__"):
