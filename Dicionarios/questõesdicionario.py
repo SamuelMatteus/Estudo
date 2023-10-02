71b9d8ac1f06868f5deae52c1d45c7be7a265ea7
Samuel Mateus <samuelmateuscardoso@gmail.com>
2023-10-02 18:27:06 -0300

    Exercícios de lista da Jornada Python

---
+++ Dicionarios/questõesdicionario.py
@@ -0,0 +1,38 @@
+#Dinâmica das questões:
+# 1- Crie um dicionário vazio chamado linguagens
+
+# 2 - Adicione as seguintes informações ao dicionário linguagens:
+# Chave: ‘Python’, Valor: ‘Linguagem de programação de alto nível
+# Chave: ‘Java’, Valor: ‘Linguagem de programação orientada a objeto’
+# Chave: ‘JavaScript’, Valor: ‘Linguagem de programação interpretada’
+
+# 3 - Acesse o valor correspondente à chave ‘Java’
+# no dicionário linguagens e armazene-o em uma variável chamada descricao_java.
+
+# 4 - Verifique se a chave ‘Python’ existe no dicionário linguagens e armazene o resultado em uma variável chamada python_existe.
+
+# 5 - Crie um novo dicionário chamado linguagens_populares com as seguintes informações:
+# Chave: ‘Python’, Valor: 2
+# Chave: ‘Java’, Valor: 1
+# Chave: ‘JavaScript’, Valor: 3
+
+#6 - Atualize o valor da chave ‘Python’ no dicionário linguagens_populares para 4.
+
+#7 - Remova a chave ‘Java’ do dicionário linguagens_populares.
+
+#8 - Crie uma variável chamada chaves e atribua a ela uma lista
+# com todas as chaves do dicionário linguagens_populares.
+
+#9 - Crie uma variável chamada valores e atribua a ela uma lista
+# com todos os valores do dicionário linguagens_populares.
+
+#10 - Crie uma variável chamada itens e atribua a ela uma lista
+# de tuplas contendo todos os itens do dicionário linguagens_populares.
+
+#11 - Crie um novo dicionário chamado linguagens_duplicadas com as seguintes informações:
+# Chave: ‘Python’, Valor: 3
+# Chave: ‘Java’, Valor: 3
+# Chave: ‘JavaScript’, Valor: 3
+
+#12 - Verifique se pelo menos um dos valores do dicionário linguagens_duplicadas
+# é igual a 5 e armazene o resultado em uma variável chamada valor_cinco_existe.
