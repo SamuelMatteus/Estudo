fad6151d2bf80ec5750563815dc86326e61c6fe5
Samuel Mateus <samuelmateuscardoso@gmail.com>
2023-10-02 19:00:38 -0300

    Exercícios de lista da Jornada Python

---
+++ Funções/exerciciosfuncoes.py
@@ -0,0 +1,75 @@
+#01 - Crie uma função chamada saudacao que recebe o nome de uma pessoa como parâmetro e imprime a seguinte mensagem:
+# “Olá, [nome]!”
+def imprimir_nome():
+    nome = str(input('Insira seu nome:'))
+    print(f"Olá, {nome}")
+
+#02 - Crie uma função chamada soma que recebe dois números como parâmetros e retorna a soma deles.
+def receber_dois_valores():
+    primeiro = float(input("Insira o primeiro valor: "))
+    segundo = float(input("Insira o segundo valor: "))
+    resultado = primeiro + segundo
+    print(resultado)
+
+#03 - Crie uma função chamada multiplicacao que recebe dois números como parâmetros
+# e retorna o resultado da multiplicação deles.
+def multiplicar_dois_valores():
+    termo1 = float(input("Insira o primeiro valor: "))
+    termo2 = float(input("Insira o segundo valor: "))
+    multiplicador = termo1 * termo2
+    print("O valor multiplicado é igual a: ", multiplicador)
+
+#04 - Crie uma função chamada media que recebe uma lista de números
+# como parâmetro e retorna a média aritmética desses números.
+def media_valores():
+    lista_valores = [2,3,4,5,6]
+    total = sum(lista_valores)
+    elementos_lista = len(lista_valores)
+    media = total / elementos_lista
+    print(media)
+
+#05 - Crie uma função chamada maior que recebe uma lista de números como parâmetro
+# e retorna o maior número presente nessa lista.
+def maior_valor():
+    lista_maior = [1,2,3,4,5,6]
+    maior = max(lista_maior)
+    print(maior)
+
+#06 - Crie uma função chamada verifica_par que recebe um número como parâmetro
+# e retorna True se o número for par e False caso contrário.
+def verificar_par():
+    numero = int(input("Insira o valor: "))
+    if numero % 2 == 0:
+        print("O número é par!")
+
+    else:
+        print("Não é par!")
+
+
+
+opcao = 0
+while opcao != 16:
+
+    print("-------------------------------")
+    print("Menu de opções: ")
+    print("1. Imprimir seu nome: ")
+    print("2. Somar dois valores")
+    print("3. Multiplicar dois valores")
+    print("4. Calcular média de valores")
+    print("5. Descobrir o maior valor de um conjunto")
+    print("6. Verificar se o número é ou não par")
+    print("-------------------------------")
+    opcao = int(input("Bem vindo! Selecione a opção conforme o menu:"))
+
+    if opcao == 1:
+        imprimir_nome()
+    elif opcao == 2:
+        receber_dois_valores()
+    elif opcao == 3:
+        multiplicar_dois_valores()
+    elif opcao == 4:
+        media_valores()
+    elif opcao == 5:
+        maior_valor()
+    elif opcao == 6:
+        verificar_par()
